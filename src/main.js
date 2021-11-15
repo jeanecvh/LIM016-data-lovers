92126ba76762a6f87662b5b8d3076c0d91371f22
Rosamaria Rodriguez <rous9716@gmail.com>
2021-11-15 01:30:29 -0500

    Feat: All Pages responsive + FilmBox

--- src/main.js
+++ src/main.js
@@ -5,18 +5,64 @@
 
 // console.log(example, data);
 
+import data from './data/ghibli/ghibli.js';
+
+
+// Event for Nav-Menu responsive
+
 const navToggle = document.querySelector(".NavToggle")
 const navMenu = document.querySelector(".nav-menu")
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-menu_visible");
+  navMenu.classList.toggle("nav-menu_visible");
+
+// Event for area-label open and close menu
 
 if (navMenu.classList.contains("nav-menu_visible")){
-    navToggle.setAttribute("aria-label", "Cerrar Menú");
+  navToggle.setAttribute("aria-label", "Close menu");
 }else {
-    navToggle.setAttribute("aria-label", "Abrir Menú")
+  navToggle.setAttribute("aria-label", "Open menu")
 }
 });
+
+//Importamos la data de Ghibli
+let films = data.films;
+export const allFilms = data.films;
+
+const filmTitles = films.map((film) => film.title);
+const filmPosters = films.map((film) => film.poster);
+
+//BUCLEANDO PARA CREAR
+function allMovies() {
+  films = data.films;
+  for (let i = 0; i < filmTitles.length; i++) {
+    //Creamos el contenedor
+    const newMovieCard = document.createElement("div");
+    newMovieCard.setAttribute("class", "moviecard");
+    //Creamos el elemento de imagen
+    const newMoviePoster = document.createElement("img");
+    newMoviePoster.setAttribute("src", filmPosters[i]);
+    newMoviePoster.setAttribute("class", "movie-poster");
+    //Creamos el elemento de titulo
+    const newMovieTitle = document.createElement("p");
+    const titleText = document.createTextNode(filmTitles[i]);
+    newMovieTitle.setAttribute("class", "filmClick");
+    //Unimos los elementos al contenedor
+    newMovieTitle.appendChild(titleText);
+    newMovieCard.appendChild(newMoviePoster);
+    newMovieCard.appendChild(newMovieTitle);
+    //Ubicamos el contenedor en el DOM
+    const newMovieContainer = document.getElementsByClassName("cards_movies")[0];
+    newMovieContainer.appendChild(newMovieCard);
+
+    document.getElementsByClassName("moviecard")[i].addEventListener("click", function () {
+      window.open("movies.html", "_self");
+      //USO LOCALSTORAGE
+      localStorage.setItem("identificador", JSON.stringify(films[i]));
+    });
+  }
+}
+allMovies();
 
 
 
